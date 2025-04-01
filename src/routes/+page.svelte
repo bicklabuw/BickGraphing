65889f07d0b26252b18599b748a7761ac1e135eb
ClearCrystals <gesparkles@gmail.com>
2025-04-01 14:16:29 -0500

    Initial commit with Svelte setup, file uploading and webgl graph.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Spectrogram from '$lib/components/spectrogram.svelte';
+	import Waveform from '$lib/components/waveform.svelte';
+</script>
+
+<h1>Welcome to Graphing Application!</h1>
+<p>Written by Grace Steinmetz</p>
+
+<Waveform />
+<Spectrogram />
