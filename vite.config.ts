02c7b59d5ec8b61d526460e7c758f83132cf632a
Kayley Seow <kayleyseow8@gmail.com>
2026-04-25 05:30:02 -0700

    edited files for united testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	optimizeDeps: {
-		exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util', 'clsx']
-	}
+    plugins: [sveltekit()],
+    optimizeDeps: {
+        exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util', 'clsx']
+    },
+    server: {
+        headers: {
+            'Cross-Origin-Embedder-Policy': 'require-corp',
+            'Cross-Origin-Opener-Policy': 'same-origin'
+        }
+    },
+    test: {
+        include: ['src/**/*.{test,spec}.{js,ts}'],
+        environment: 'node',
+        globals: false
+    }
 });
